ebff544efa9d38bc711c824c3b2150df977ae796
Mihir Pokhriyal <mihirpokhriyal@Mihirs-MacBook-Air.local>
2021-02-13 14:57:00 -0500

    home and settings collab configured

---
+++ app/src/main/java/com/example/exercise/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openShop(View view) {
+        Intent intent = new Intent(this, ShopActivity.class);
+        startActivity(intent);
+
+    }
+    public void openSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPlay (View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSocial(View view) {
+        Intent intent = new Intent(this, SocialActivity.class);
+        startActivity(intent);
+    }
+}
