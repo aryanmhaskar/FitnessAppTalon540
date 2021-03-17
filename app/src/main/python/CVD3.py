beb6c57c2fbdda7f74b39f96bf253881dc5b6fbf
aryanmhaskar <aryanmhaskar@hotmail.com>
2021-03-17 17:44:18 -0400

    Camera tracking implementation with a few errors, to be fixed next push

--- app/src/main/python/CVD3.py
+++ app/src/main/python/CVD3.py
@@ -58,13 +58,8 @@ def poseDetector(frame):
         assert(partFrom in bodyParts)
         assert(partTo in bodyParts)
 
-        idFrom = bodyParts[partFrom]
-        idTo = bodyParts[partTo]
 
-        if points[idFrom] and points[idTo]:
-            cv2.line(frame, points[idFrom], points[idTo], (0, 250, 0), 5)
-            cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (225, 0, 0), cv2.FILLED)
-            cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (225, 0, 0), cv2.FILLED)
+
 
     t, _ = convNet.getPerfProfile()
     return frame
@@ -96,29 +91,9 @@ def computer_vision_module():
             minNeighbors=1,     
             minSize=(20, 20)
         )
-        
-        for (x,y,w,h) in faces:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),3)
-                roi_gray = img[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]  
-            
-        for (x,y,w,h) in lower:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
-                roi_gray = img[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]
-        
-        for (x,y,w,h) in upper:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,300),3)
-                roi_gray = img[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]
-            
-        for (x,y,w,h) in bodies:
-                cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-                roi_gray = img[y:y+h, x:x+w]
-                roi_color = img[y:y+h, x:x+w]   
+          
             
         img = poseDetector(img)
-        cv2.imshow('video',img)
         k = cv2.waitKey(30) & 0xff
         if k == 27: # press 'ESC' to quit
             break
